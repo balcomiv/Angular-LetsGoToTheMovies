43354f1f21eeebbcec74ef9c5c33df861a8ef9cb
balcomiv <35431931+balcomiv@users.noreply.github.com>
2019-06-04 05:43:38 -0400

    Add project files.

---
+++ Angular-LetsGoToTheMovies/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularLetsGoToTheMovies.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Director { get; set; }
+    }
+}
